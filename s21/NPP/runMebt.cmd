08ae2af283795deadf1137eeec0601ff7f8cbacb
Zoltán Gáspár <zoltan.gaspar@mobilengine.com>
2014-07-18 11:27:44 +0000

    fixing path env var

--- s21/NPP/runMebt.cmd
+++ s21/NPP/runMebt.cmd
@@ -3,8 +3,8 @@
 SET MEBT_PASSWORD=wapaja.115
 SET MEBT_SERVICEURL=http://localhost:8081/Services/Comex
 SET MEBT_USER=su
-SET MEBT_PATH=c:\work\projects\ME-server\trunk\MobileServer
+SET MEBT_PATH=c:\work\projects\ME-server\trunk\MobileServer\ComexClient\bin\Debug
 
 cd %1
 for %%F in ("%cd%") do set "folder=%%~nxF"
-%MEBT_PATH%\ComexClient\bin\Debug\Mebt.exe run --no-header -c %folder% . rtab.xlsx
+%MEBT_PATH%\Mebt.exe run --no-header -c %folder% . rtab.xlsx
