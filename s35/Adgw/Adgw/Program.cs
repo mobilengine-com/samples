20315ed458398334936bb37fcc5dfe04be00bb84
Zoltán Gáspár <zoltan.gaspar@mobilengine.com>
2017-05-02 16:20:32 +0000

    adgw initial check in reads both ad and me users but only only user creating works

---
+++ s35/Adgw/Adgw/Program.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.DirectoryServices.Protocols;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ADGW
+{
+    class AdattrAttribute : Attribute
+    {
+        public readonly string stAttr;
+        public readonly bool fMulti;
+
+        public AdattrAttribute(string stAttr, bool fMulti = false)
+        {
+            this.stAttr = stAttr;
+            this.fMulti = fMulti;
+        }
+    }
+
+
+    class Aduser
+    {
+        [Adattr("userprincipalname")] //string-unicode
+        public string userprincipalname { get; set; }
+
+        [Adattr("usnchanged")] // interval == largeInteger 
+        public string usnchanged { get; set; }
+        [Adattr("whenchanged")] // String(Generalized-Time) "YYYYMMDDHHMMSS.0Z"
+        public string whenchanged { get; set; }
+        [Adattr("pwdlastset")] // interval == largeInteger  
+        public string pwdlastset { get; set; }
+        [Adattr("objectGUID")] // Object(Replica-Link) - System.Byte[]
+        public string objectGUID { get; set; }
+        [Adattr("SAMAccountName")] // String(Unicode)
+        public string SAMAccountName { get; set; }
+        [Adattr("memberOf", fMulti: true)] // Object(DS-DN)
+        public string[] memberOf { get; set; }
+        [Adattr("cn")] // common name, String(Unicode)
+        public string cn { get; set; }
+        [Adattr("displayname")] // String(Unicode)
+        public string displayname { get; set; }
+        [Adattr("mail")] // String(Unicode)
+        public string mail { get; set; }
+        [Adattr("telephonenumber")] // String(Unicode)
+        public string telephonenumber { get; set; }
+        [Adattr("mobile")] // String(Unicode)
+        public string mobile { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("userprincipalname: {0}, usnchanged: {1}, whenchanged: {2}, pwdlastset: {3}, objectGUID: {4}, SAMAccountName: {5}, memberOf: {6}, cn: {7}, displayname: {8}, mail: {9}, telephonenumber: {10}, mobile: {11}", userprincipalname, usnchanged, whenchanged, pwdlastset, objectGUID, SAMAccountName, memberOf == null ? "" : String.Join(", ", memberOf), cn, displayname, mail, telephonenumber, mobile);
+        }
+
+        public void ParseFromEntry(SearchResultEntry entry)
+        {
+            foreach (var propertyInfo in GetType().GetProperties())
+            {
+                var adattr = (AdattrAttribute)propertyInfo.GetCustomAttributes(typeof(AdattrAttribute), false).First();
+                if (adattr != null)
+                {
+                    if (entry.Attributes[adattr.stAttr] != null)
+                    {
+                        if (!adattr.fMulti)
+                        {
+                            var o = entry.Attributes[adattr.stAttr][0];
+                            if (o is byte[])
+                            {
+                                propertyInfo.SetValue(this, new Guid((byte[]) o).ToString());
+                            }
+                            else
+                            {
+                                propertyInfo.SetValue(this, o.ToString());
+                            }
+                        }
+                        else
+                        {
+                            var rgst = Enumerable.Range(0, entry.Attributes[adattr.stAttr].Count).Select(i => entry.Attributes[adattr.stAttr][i].ToString()).ToArray();
+                            propertyInfo.SetValue(this, rgst);
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    internal class Program
+    {
+
+
+        private static void Main(string[] args)
+        {
+            var domain = ConfigurationManager.AppSettings["ad:domain"];
+
+            var ldapDirectoryIdentifier = new LdapDirectoryIdentifier(domain);
+
+            var networkCredential = new NetworkCredential(ConfigurationManager.AppSettings["ad:lookupUser"], ConfigurationManager.AppSettings["ad:lookupUserPwd"]);
+
+            var connection = new LdapConnection(ldapDirectoryIdentifier) {AuthType = AuthType.Basic};
+
+            try
+            {
+                connection.Bind(networkCredential);
+            }
+            catch (Exception exception)
+            {
+                Trace.WriteLine(exception.ToString());
+            }
+
+            connection.SessionOptions.ProtocolVersion = 3;
+
+            var request = new SearchRequest
+                ("DC=meadfstest,DC=local",
+                    "(&(objectClass=person)(objectClass=user)(memberOf={0}))".StFormat(ConfigurationManager.AppSettings["ad:userGroupDistinguishedName"]), 
+                    SearchScope.Subtree,
+                    new[] { "userprincipalname", "usnchanged", "whenchanged", "pwdlastset", "objectGUID", "SAMAccountName", "memberOf", "cn", "displayname", "mail", "telephonenumber", "mobile" });
+
+
+            var searchOptions = new SearchOptionsControl(SearchOption.DomainScope);
+            request.Controls.Add(searchOptions);
+            
+            var pageResultRequestControl = new PageResultRequestControl(1000);
+            request.Controls.Add(pageResultRequestControl);
+            var rgadusr = new List<Aduser>();
+            while (true)
+            {
+                var searchResponse = (SearchResponse) connection.SendRequest(request);
+                var pageResponse = (PageResultResponseControl) searchResponse.Controls[0];
+
+
+                foreach (SearchResultEntry entry in searchResponse.Entries)
+                {
+                    var aduser = new Aduser();
+                    aduser.ParseFromEntry(entry);
+                    if (!string.IsNullOrEmpty(aduser.userprincipalname))
+                        rgadusr.Add(aduser);   
+                }
+
+                if (pageResponse.Cookie.Length == 0)
+                    break;
+
+                pageResultRequestControl.Cookie = pageResponse.Cookie;
+
+            }
+
+            foreach (var adusr in rgadusr)
+            {
+                Console.WriteLine(adusr);
+            }
+            Console.WriteLine("-- end of users --");
+
+            var uman = new Uman(ConfigurationManager.AppSettings["me:urlUman"], ConfigurationManager.AppSettings["me:usernpwdDev"]);
+
+            var jsonUsers = uman.WbReqst("users", "{{ companyId: {0} }}".StFormat(ConfigurationManager.AppSettings["me:companyId"]))
+                .GetResponseStream().StReadAsUtf8().GetJson();
+            Console.WriteLine( jsonUsers  );
+            Console.WriteLine("-- end of response --");
+
+            var rgmeusers = new HashSet<string>();
+            foreach (var jtoken in jsonUsers["result"])
+            {
+                Console.WriteLine("usern: {0}", jtoken["email"]);
+                Console.WriteLine("issuer: {0}", jtoken.Value<string>("idpIssuer"));
+                if (jtoken.Value<string>("idpIssuer") == ConfigurationManager.AppSettings["me:issuer"])
+                {
+                    rgmeusers.Add(jtoken.Value<string>("email"));
+                }
+            }
+
+            foreach (var aduser in rgadusr)
+            {
+                if (!rgmeusers.Contains(aduser.userprincipalname))
+                {
+                    // create
+                    Console.WriteLine("creating user {0}", aduser.userprincipalname);
+                    var createuserpars = JsonConvert.SerializeObject(Umanusr.UmanusrFromAduser(aduser), formatting: Formatting.Indented);
+                    Console.WriteLine(createuserpars);
+                    Console.WriteLine("creating user: {0}".StFormat(
+                            uman.WbReqst("createuser",
+                                createuserpars
+                            ).GetResponseStream().StReadAsUtf8().GetJson()
+                        ));
+
+                }
+                else
+                {
+                    // alter
+                    Console.WriteLine("altering user {0}", aduser.userprincipalname);
+                }
+                rgmeusers.Remove(aduser.userprincipalname);
+            }
+
+            foreach (var usernToDel in rgmeusers)
+            {
+                // delete
+                Console.WriteLine("deleting user {0}", usernToDel);
+            }
+
+            Console.WriteLine("-- end of modifications --");
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        //
+        // for debug purposes - display all info of AD object
+        private static void MainX(string[] args)
+        {
+            var domain = "argus.mebt";
+
+            var ldapDirectoryIdentifier = new LdapDirectoryIdentifier(domain);
+
+            var networkCredential = new NetworkCredential("zgaspar", "wapaja.115");
+
+            var connection = new LdapConnection(ldapDirectoryIdentifier) {AuthType = AuthType.Basic};
+
+            try
+            {
+                connection.Bind(networkCredential);
+            }
+            catch (Exception exception)
+            {
+                Trace.WriteLine(exception.ToString());
+            }
+
+            connection.SessionOptions.ProtocolVersion = 3;
+
+            var request = new SearchRequest
+                ("DC=meadfstest,DC=local",
+                    "(&(objectClass=person)(objectClass=user)(memberOf=CN=mobilengine,CN=Users,DC=meadfstest,DC=local))", // (&(objectClass=person)(objectClass=user)) (memberOf=CN=mobilengine,CN=Users,DC=meadfstest,DC=local)  (objectClass=group)
+                    SearchScope.Subtree,
+                    null);
+
+
+            var searchOptions = new SearchOptionsControl(SearchOption.DomainScope);
+            request.Controls.Add(searchOptions);
+
+            var pageResultRequestControl = new PageResultRequestControl(1000);
+            request.Controls.Add(pageResultRequestControl);
+
+            while (true)
+            {
+                var searchResponse = (SearchResponse) connection.SendRequest(request);
+                var pageResponse = (PageResultResponseControl) searchResponse.Controls[0];
+
+
+                foreach (SearchResultEntry entry in searchResponse.Entries)
+                {
+                    if (entry.Attributes.Contains("sAMAccountName") && entry.Attributes["sAMAccountName"][0].ToString() != String.Empty)
+                    {
+                        Console.WriteLine(entry.DistinguishedName);
+                        foreach (var attr in entry.Attributes)
+                        {
+                            var x = (DictionaryEntry)attr;
+                            var directoryAttribute = entry.Attributes[(string)x.Key];
+                            foreach (var y in Enumerable.Range(0, directoryAttribute.Count))
+                            {
+                                Console.WriteLine("  " + x.Key + " / " + directoryAttribute[y]);
+                            }
+                        }
+                    }
+                }
+
+                if (pageResponse.Cookie.Length == 0)
+                    break;
+
+                pageResultRequestControl.Cookie = pageResponse.Cookie;
+
+            }
+
+
+            Console.WriteLine("-- end of users --");
+            Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ s35/Adgw/Adgw/Uman.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ADGW
+{
+
+
+    class Umanusr
+    {
+        public Umanusr()
+        {
+            hwValidation = false;
+            fSendEmail = false;
+            fSendSms = false;
+        }
+
+        public string email { get; set; }
+        public string idpIssuer { get; set; }
+        public int companyId { get; set; }
+        public string userName { get; set; }
+        public string mobileNumber { get; set; }
+        public string mobilePlatform { get; set; }
+        public bool hwValidation { get; set; }
+        public string role { get; set; }
+        public bool webformAccess { get; set; }
+        public bool dashboardAccess { get; set; }
+        public bool fSendEmail { get; set; }
+        public bool fSendSms { get; set; }
+
+        public static Umanusr UmanusrFromAduser(Aduser aduser)
+        {
+            return new Umanusr
+            {
+                email = aduser.userprincipalname,
+                idpIssuer = ConfigurationManager.AppSettings["me:issuer"],
+                companyId = int.Parse(ConfigurationManager.AppSettings["me:companyId"]),
+                userName = aduser.displayname,
+                mobileNumber = aduser.mobile,
+                mobilePlatform = "ios",
+                role = "companyadmin",
+                webformAccess = true,
+                dashboardAccess = true
+            };
+        }
+    }
+    /// <summary>
+    /// Indicates that the marked method builds string by format pattern and (optional) arguments.
+    /// Parameter, which contains format string, should be given in constructor. The format string
+    /// should be in <see cref="string.Format(IFormatProvider,string,object[])"/>-like form
+    /// </summary>
+    /// <example><code>
+    /// [StringFormatMethod("message")]
+    /// public void ShowError(string message, params object[] args) { /* do something */ }
+    /// public void Foo() {
+    ///   ShowError("Failed: {0}"); // Warning: Non-existing argument in format string
+    /// }
+    /// </code></example>
+    [AttributeUsage(
+      AttributeTargets.Constructor | AttributeTargets.Method,
+      AllowMultiple = false, Inherited = true)]
+    public sealed class StringFormatMethodAttribute : Attribute
+    {
+        /// <param name="formatParameterName">
+        /// Specifies which parameter of an annotated method should be treated as format-string
+        /// </param>
+        public StringFormatMethodAttribute(string formatParameterName)
+        {
+            FormatParameterName = formatParameterName;
+        }
+
+        public string FormatParameterName { get; private set; }
+    }
+
+    public static class Ext
+    {
+
+/*        public static HttpStatusCode GetStatusCode(this string stC)
+        {
+            var htsc = stC.ParseKOrNil<HttpStatusCode>();
+            Assert.That(htsc != null, "Not A Valid Http Status Code: '{0}'".StFormat(stC));
+            return htsc.Value;
+        }*/
+
+        [StringFormatMethod("stFormat")]
+        public static string StFormat(this string stFormat, params object[] args)
+        {
+            return String.Format(stFormat, args);
+        }
+
+        public static JObject GetJson(this string stJ)
+        {
+            var jO = JsonConvert.DeserializeObject<JObject>(stJ);
+            var jTmsg = jO["message"];
+            if (jTmsg != null)
+                jO.Property("message").Value = jTmsg.Value<string>().Split('\n')[0];
+            var jTRst = jO["result"];
+            if (jTRst != null && jTRst.HasValues)
+            {
+                if (jO.SelectToken("result.created") != null)
+                    jO.SelectToken("result.created").Parent.Remove();
+            }
+            return jO;
+        }
+
+        public static bool FSuccess(this JObject jO)
+        {
+            if (jO["resultCode"] == null)
+                return false;
+            return jO["resultCode"].Value<int>() == 0;
+        }
+
+        public static int GetResultId(this JObject jO)
+        {
+/*            Assert.That(jO["result"] != null, Is.True,
+                "Json Does Not Contains Result.\n The message was: {0}".StFormat(jO.Property("message").Value));*/
+            return jO.SelectToken("result").Value<int>();
+        }
+
+        public static string StReadAsUtf8(this Stream strm)
+        {
+            using (var sr = new StreamReader(strm, Encoding.UTF8))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+
+    public class Uman
+    {
+
+        public Dictionary<string, int> mpIdByStName = new Dictionary<string, int>();
+        private readonly Regex reg = new Regex(@"(#[^.]*#)");
+        private string urlUman;
+        private string stUsrnPwd;
+
+        public Uman(string urlUman, string stUsrnPwd)
+        {
+            this.urlUman = urlUman;
+            this.stUsrnPwd = stUsrnPwd;
+        }
+
+        public HttpWebResponse WbReqst(string action, string stJson)
+        {
+            var url = urlUman + action;
+            Console.WriteLine(url);
+            var req = (HttpWebRequest)WebRequest.Create(url);
+
+            var rgUsr = stUsrnPwd.Split('/');
+            var encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(rgUsr[0] + ":" + rgUsr[1]));
+            req.Headers.Add("Authorization", "Basic " + encoded);
+
+            stJson = reg.Replace(stJson, match => mpIdByStName[match.Value].ToString());
+
+            var postData = stJson;
+            var data = Encoding.UTF8.GetBytes(postData);
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            req.ContentLength = data.Length;
+
+            using (var stream = req.GetRequestStream())
+                stream.Write(data, 0, data.Length);
+
+            try
+            {
+                return (HttpWebResponse)req.GetResponse();
+            }
+            catch (WebException wer)
+            {
+                if (wer.Response != null)
+                    return (HttpWebResponse)wer.Response;
+
+                throw;
+            }
+        }
+
+        /*
+        public int GetRespValue(HttpWebResponse hwresp)
+        {
+            var stResp = StReadAsUtf8(hwresp.GetResponseStream());
+            Assert.That(hwresp.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Unexpected reponse: {0}({1}\n{2}",
+                hwresp.StatusCode, hwresp.StatusDescription, stResp);
+
+            return stResp.GetJson().GetResultId();
+        }
+
+        public void CheckErrors(string sthwr, Table t)
+        {
+            var rgStErrFromSthwr = sthwr.Split('\n');
+            Assert.That(rgStErrFromSthwr.Count() == t.RowCount, "Count Mismatch Expected: {0} , But Was: {1}".StFormat(t.RowCount, rgStErrFromSthwr.Count()));
+            for (var i = 0; i < t.Rows.Count; i++)
+                Assert.That(rgStErrFromSthwr[i], Is.EqualTo(t.Rows[i].Values.First()));
+        }
+
+        public string ReplaceFromMp(string stJson)
+        {
+            var match = reg.Match(stJson);
+            if (!match.Success)
+                return stJson;
+            var stMatch = match.Value;
+            stJson = stJson.Replace("\"{0}\"".StFormat(stMatch), mpIdByStName[stMatch].ToString());
+            return stJson;
+        }
+        */
+
+
+    }
+
+}
