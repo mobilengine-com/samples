221d5f5a0f7d3cbc2d84e1849e3c2d95af79f060
Zoltán Gáspár <zoltan.gaspar@mobilengine.com>
2017-06-08 13:57:20 +0000

    adgw create default iep from bat

---
+++ s35/adgwSolution/create_iep.bat
@@ -0,0 +1,3 @@
+SET DBFAIRY_PATH=c:\work\projects\ME-server\trunk\MobileServer\Dbfairy\bin\x86\Debug\
+echo %DATE% %TIME%
+%DBFAIRY_PATH%\dbfairy.exe -iep:inUserFromAD -kiep:Wdi -key:51bf805a65454763ac5dc91e847de5e9 -company=Gringotts
