03630b697831a842823f1083ef807975e12a9fff
Zoltán Gáspár <zoltan.gaspar@mobilengine.com>
2017-07-04 14:06:30 +0000

    link to ps ad commands

--- s35/ad-ps1/ad-testusers.ps1
+++ s35/ad-ps1/ad-testusers.ps1
@@ -1,4 +1,6 @@
-﻿for ($i=200; $i -lt 300; $i++) { 
+﻿# ps ad commands: https://technet.microsoft.com/en-us/library/ee617195.aspx
+
+for ($i=200; $i -lt 300; $i++) { 
 	$usern = "testuser"+$i 
 	New-ADUser -name $usern -userprincipalname ($usern + "@meadfstest.local") -DisplayName ("Test User " + $i) -EmailAddress ("dev+testuser" + $i + "@mobilengine.com") -MobilePhone ("+3632112" + $i)
 	Set-ADAccountPassword $usern -newpassword (ConvertTo-SecureString -AsPlainText "Vujmag16" -Force)
