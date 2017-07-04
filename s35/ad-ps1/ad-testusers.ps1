9ad511d0edfb1a23f3de8c7a726595a12d40daf8
Zoltán Gáspár <zoltan.gaspar@mobilengine.com>
2017-07-04 09:25:13 +0000

    powershell script to create test ad users

---
+++ s35/ad-ps1/ad-testusers.ps1
@@ -0,0 +1,6 @@
+﻿for ($i=200; $i -lt 300; $i++) { 
+	$usern = "testuser"+$i 
+	New-ADUser -name $usern -userprincipalname ($usern + "@meadfstest.local") -DisplayName ("Test User " + $i) -EmailAddress ("dev+testuser" + $i + "@mobilengine.com") -MobilePhone ("+3632112" + $i)
+	Set-ADAccountPassword $usern -newpassword (ConvertTo-SecureString -AsPlainText "Vujmag16" -Force)
+	Add-ADGroupMember mobilengine $usern
+}
