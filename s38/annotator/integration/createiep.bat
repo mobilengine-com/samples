93a50dcd0087d5fbc584219fe1ecdfb51d28d93a
Zoltán Gáspár <zoltan.gaspar@mobilengine.com>
2018-10-31 13:55:19 +0000

    annotator & fileview control samples

---
+++ s38/annotator/integration/createiep.bat
@@ -0,0 +1,2 @@
+Dbfairy.exe -iep:in -kiep:Wdi -key c66bf4c3fcbe4934bc0b3315b1dcac3e -company Gringotts
+Dbfairy.exe -iep:out -kiep:Wdo -key c66bf4c3fcbe4934bc0b3315b1dcac3f -company Gringotts -url http://alma.hu
