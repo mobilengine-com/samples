3b6ed0035b48d1486bb20ee06474a0331e0aef35
Tamás Kovács <tamas.kovacs@mobilengine.com>
2023-02-17 11:14:56 +0100

    Add typescript hello world sample

---
+++ s50/typescript/hello_world.ts
@@ -0,0 +1,7 @@
+//# server typescript program hello_world for schedule * * * * * first run at 2100-01-01 00:00
+
+{
+    var hello = `Hello`;
+    var world = `World`;
+    Log(`${hello} ${world}!`)
+}
