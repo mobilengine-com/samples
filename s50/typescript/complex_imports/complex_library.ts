331f17f670e2a36dcc63d0c4b31f3fcd91eeeef5
Tamás Kovács <tamas.kovacs@mobilengine.com>
2023-02-17 13:56:49 +0100

    Complex ts import: add default export sample

--- s50/typescript/complex_imports/complex_library.ts
+++ s50/typescript/complex_imports/complex_library.ts
@@ -1,5 +1,8 @@
 //# server typescript program complex_library for schedule * * * * * first run at 2100-01-01 00:00
 
+// See the docs here:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export
+
 // export a declaration
 export function libraryFunction() {
     return "value from imported function";
@@ -12,6 +15,15 @@ export { anotherConstant };
 
 // export with a different name
 export { anotherConstant as piApproximation };
+
+const defaultExport = {
+    someValue: 4242,
+    someFunction: function () {
+        return "SomeFunction called"
+    }
+};
+
+export default defaultExport;
 
 Log("Watch out!");
 Log("This top level code executes once when this library is imported");
