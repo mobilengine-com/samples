10c0d6c4ec28fa26ad3ff92d275af013bf9f2742
Botond Portik-Bakai <botond.bakai@mobilengine.com>
2023-03-23 15:31:15 +0100

    Updated for better look and understandability

--- s50/pdf-render/renderPdf.ts
+++ s50/pdf-render/renderPdf.ts
@@ -1,13 +1,16 @@
 //# server typescript program renderPdf for form pdf_render
 //#using reftab images;
 {
-	let validPdf = pdf.FromFileref(fileref.New(form.up_load1.files[0].mediaId, 0));
-	var filename = form.up_load1.files[0].name;
-	var page = parseInt(form.page.text);
-	var width = parseInt(form.width.text);
-	var height = parseInt(form.height.text);
-	var imagePdf = validPdf.Render(page, width, height);
-	var mediaId = imagePdf.Store("png");
+	let firstUploadedfileMediaId = form.up_load1.files[0].mediaId;
+	let pdfileref = fileref.New(firstUploadedfileMediaId, 0);
+	let filename = form.up_load1.files[0].name;
+	let page = parseInt(form.page.text);
+	let width = parseInt(form.width.text);
+	let height = parseInt(form.height.text);
+
+	let validPdf = pdf.FromFileref(pdfileref);
+	let imagePdf = validPdf.Render(page, width, height);
+	let mediaId = imagePdf.Store("png");
 	Log("Generate image with mediaId: " + mediaId);
 	db.images.Insert({mediaId:mediaId, name: filename.split('.')[0], page: form.page.text, width: form.width.text, height: form.height.text});
 }
