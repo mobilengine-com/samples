dee3b66faff58a4b07d87c9c25a8b7c945ce3e4f
Tamás Kovács <tamas.kovacs@mobilengine.com>
2023-11-30 15:46:38 +0100

    Adding ios typescript sample

---
+++ s52/client-typescript/mywebscript.ts
@@ -0,0 +1,7 @@
+//# client typescript program mywebscript for form mywebform
+
+import { myFunction } from './library';
+
+Log(ToString(JSON.stringify({ "hello": "world", "the number": myFunction() })));
+
+Log(form);
