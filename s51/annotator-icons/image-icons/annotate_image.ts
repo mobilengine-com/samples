72cfd399efa4b704e3fe7b8a55a6f420784f833c
Tamás Kovács <keccsx@gmail.com>
2023-12-11 08:43:58 +0100

    s51 annotate image: Improve var naming

--- s51/annotator-icons/image-icons/annotate_image.ts
+++ s51/annotator-icons/image-icons/annotate_image.ts
@@ -2,27 +2,27 @@
 
 //# using reftab images;
 
-function DrawShapeWithIcon(canvas, image, centerX, centerY, size) {
+function DrawShapeWithIcon(canvas, icon, centerX, centerY, size) {
     canvas.DrawShape(centerX, centerY, size, {
         shape: "disk", color: "#BF360C"
     });
 
-    let resizedImage = image.Clone();
-    resizedImage.Resize(size, size);
-    canvas.DrawImage(resizedImage, centerX - size / 2, centerY - size / 2);
-    resizedImage.Close();
+    let resizedIcon = icon.Clone();
+    resizedIcon.Resize(size, size);
+    canvas.DrawImage(resizedIcon, centerX - size / 2, centerY - size / 2);
+    resizedIcon.Close();
 }
 
 {
     db.images.DeleteMany(map.New());
 
-    let blank = image.FromIcon("blank");
+    let canvas = image.FromIcon("blank");
     let icon = image.FromIcon("corrected");
 
-    DrawShapeWithIcon(blank, icon, 50, 50, 10);
-    DrawShapeWithIcon(blank, icon, 100, 100, 50);
-    DrawShapeWithIcon(blank, icon, 300, 300, 200);
+    DrawShapeWithIcon(canvas, icon, 50, 50, 10);
+    DrawShapeWithIcon(canvas, icon, 100, 100, 50);
+    DrawShapeWithIcon(canvas, icon, 300, 300, 200);
 
-    let mediaId = blank.Store("png");
+    let mediaId = canvas.Store("png");
     db.images.Insert({ image: fileref.New(mediaId, 0) });
 }
