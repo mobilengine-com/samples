b7fdb099f428c46e68a1cdd559465c620214c36f
Zoltán Gáspár (Roger) <zoltan.gaspar@mobilengine.com>
2024-02-14 17:23:14 +0100

    for spreadsheet new features

---
+++ s52/spreadsheet/producersWeb.ts
@@ -0,0 +1,10 @@
+﻿//# server typescript program producersWeb for form producersWeb
+//# using reftab producer;
+
+{
+  for (let row of form.tblProducer.rows) {
+    if (row.letProducer.guid == null) {
+      db.producer.Insert({guid: guid.Generate().ToStringN(), name: row.letProducer.name, description: row.letProducer.description});
+    }
+  }
+}
---
+++ s52/spreadsheet/productsWeb.ts
@@ -0,0 +1,32 @@
+﻿//# server typescript program productsWeb for form productsWeb
+//# using reftab product;
+//using dacs ProductVignetteRequest as req;
+
+{
+  Log([form]);
+  if (form.submitButton === form.btnSave) {
+    for (let row of form.tblProduct.rows) {
+      if (row.letFNew) {
+        let product = {
+          guid: guid.Generate().ToStringN(), 
+          name: row.tbName.text, 
+          guidProducer: row.ddProducer.selectedKey,
+          itemNumber: row.tbItemNumber.text,
+          dtlIntroduced: row.dpDtlIntroduced.date?.DtlToDtdb(),
+          urlDatasheet: row.tbUrlDatasheet.text,
+          urlDoP: row.tbUrlDoP.text,
+          urlBIMObject: row.tbUrlBIMObject.text,
+          urlOther: row.tbUrlOther.text,
+          tsMinSomething: row.tpTsMinSomething.seconds === null ? null : row.tpTsMinSomething.seconds/60,
+          filerefVignette: null
+        };
+        db.product.Insert(product);
+/*
+        var reqDacs = messages.req.New();
+
+        reqDacs.Product.guid = product.guid;
+        reqDacs.Send();*/
+      }
+    }
+  }
+}
