6c4ccce79bd7c0a307790513156734751d35e3ee
Tamás Kovács <tamas.kovacs@mobilengine.com>
2024-06-19 14:41:28 +0200

    Add a clarifying comment to narrowing

--- s51/rfs-http-request/get_me_version.ts
+++ s51/rfs-http-request/get_me_version.ts
@@ -6,6 +6,8 @@ const request = new HttpRequest();
 request.url = 'https://login.bauapp.hu/services/comex/v1/version';
 const response = request.getTextResponse();
 if ('error' in response) {
+	// this is type-safe, typescript knows that response is a HttpError
+	// see https://www.typescriptlang.org/docs/handbook/2/narrowing.html
 	Log(response.error);
 	throw new Error('The request failed');
 }
