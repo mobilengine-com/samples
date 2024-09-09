430a5a9d59377a96a37ef129396a9ffca740bca7
Tamás Kovács <tamas.kovacs@mobilengine.com>
2024-09-09 15:37:08 +0200

    Bim demo: model-blueprint matching

--- s54/bim-demo/matching/matchingScript.ts
+++ s54/bim-demo/matching/matchingScript.ts
@@ -2,9 +2,36 @@
 
 //# using reftab matchings;
 
+interface Matching {
+    blueprintId: string,
+    modelId: string,
+    storeyId: string,
+    storeyName: string,
+    imagePicks: string,
+    modelPicks: string,
+    scale: number[],
+    rotation: number[],
+    position: number[],
+    eyeLevel: number,
+    eyeLevelSource: string
+}
+
 if (form.submitButton === form.saveMatching) {
+    const matching = JSON.parse(form.matcher.matchData) as Matching;
+    const id = `${matching.blueprintId}-${matching.modelId}-${matching.storeyId}`;
+    const displayName =
+        'Model: "' + form.modelDropdown.selectedText +
+        '" Storey: "' + matching.storeyName +
+        '" Blueprint: "' + form.blueprintDropdown.selectedText +
+        '"';
+
+    db.matchings.DeleteMany({
+        id: id
+    });
+
     db.matchings.Insert({
-        id: guid.Generate().ToStringN(),
+        id: id,
+        displayName: displayName,
         modelId: form.modelDropdown.selectedKey,
         blueprintId: form.blueprintDropdown.selectedKey,
         matchData: form.matcher.matchData
