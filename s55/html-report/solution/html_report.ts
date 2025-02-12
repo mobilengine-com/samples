f8ce1a41ba92fe80a222e3b5a369f02bea0563eb
Tamás Kovács <tamas.kovacs@mobilengine.com>
2025-02-12 04:04:52 +0100

    Update html report sample

--- s55/html-report/solution/html_report.ts
+++ s55/html-report/solution/html_report.ts
@@ -2,7 +2,6 @@
 // Code to build the report HTML from the report input.
 // ------------------------------------------------------------------------------------------------
 
-import { html } from './mobilengine'
 import { libreBaskervilleFont } from './resources'
 
 export interface Photo {
