9cad884447dc00c24b7a3f909ca6ea211d8637ef
zoltan.gaspar@mobilengine.com <zoltan.gaspar@mobilengine.com>
2025-03-13 10:22:28 +0100

    s55 samples

---
+++ s55/form-ts-types/OnFileUpload.ts
@@ -0,0 +1,22 @@
+﻿//# server typescript program OnFileUpload for form UploadFile
+{
+	const tform = form as form_UploadFile;
+	let mediaidSignature = tform.sig.binaryId;
+	Log(["signature id:", mediaidSignature])
+
+	Log(["submitter:", tform.info.user.name])
+	Log(["loc:", tform.info.location])
+
+	let photos = tform.photo.photos;
+	for(let photo of photos) {
+		Log(["photo id", photo.photoId])
+	}
+
+	for (let i=0; i<tform.fileupload.filerefs.Count(); i=i+1) {
+		let fr = tform.fileupload.filerefs[i].fileref;
+		let file = tform.fileupload.files[i];
+		Log(["fileref", fr, fr.MediaId(), fr.Size(), fr.Filename(), fr.Checksum(), fr.Priority(), "file", file, "toString", fr.ToString()])
+	}
+	Log(form)
+
+}
---
+++ s55/form-ts-types/ios_photo_signature.ts
@@ -0,0 +1,17 @@
+﻿//# server client typescript program ios_photo_signature for form ios_photo_signature
+{
+	const tform = form as form_ios_photo_signature;
+	let mediaidSignature = tform.signature.binaryId;
+	Log(["signature id:", mediaidSignature])
+
+	Log(["submitter:", tform.info.user.name])
+	Log(["loc:", tform.info.location])
+
+	let photos = tform.photo.photos;
+	for(let photo of photos) {
+		Log(["photo id", photo.photoId])
+	}
+
+	Log(form)
+
+}
