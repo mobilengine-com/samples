41b327435c81cf634a680450a8f59005b06031b5
Tamás Kovács <tamas.kovacs@mobilengine.com>
2025-06-12 15:52:58 +0200

    rfs descending order by sample

---
+++ s57/rfs-orderby-desc/RtabOrderLimitOffset.ts
@@ -0,0 +1,22 @@
+﻿//# server typescript program RtabOrderLimitOffset for schedule * * * * * first run at 2100-01-01 10:00
+//# using reftab FRUIT500;
+
+Log("cheapest fruits first (long version)");
+Log(db.FRUIT500.Read({
+	_orderBy: [
+		{
+			column: "Price",
+			direction: "desc"
+		},
+		"Fruit"
+	]
+}));
+
+Log("cheapest fruits first (short version)");
+Log(db.FRUIT500.Read({
+	_orderBy: [
+		// this function just returns the same object that you see above
+		db.desc("Price"),
+		"Fruit"
+	]
+}));
