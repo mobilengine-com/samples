8fd86d2c7f058419942a462b90b0b8ba49dd9eed
zoltan.gaspar@mobilengine.com <zoltan.gaspar@mobilengine.com>
2025-06-17 09:36:49 +0200

    shared lock sample updated

--- s56/lock-shared/accounts.ts
+++ s56/lock-shared/accounts.ts
@@ -12,12 +12,13 @@
 		//trace row.rec, rgth;  
 		if (rgth.Count() == 0) {
 			let acc = {
-				acc_num: row.recUpd.acc_num, holder_name: row.recUpd.holder_name, balance: row.recUpd.balance,
+				acc_num: row.recUpd.acc_num, holder_name: row.recUpd.holder_name, 
 				valid: row.recUpd.valid, ver: 0
 			};
 			Log(["new account", acc]);
 
 			db.account.Insert(acc);
+			db.balance.Insert({acc_num: acc.acc_num, balance: row.recUpd.balance, ver: 0});
 		}
 	}
 
@@ -31,6 +32,9 @@
 	Log(`waited ${tform.nWaitLocked.number}`);
 
 	for (let row of tform.t.rows) {
+		Log(row)
+		if (!row.fChanged)
+			continue;
 		let th = db.account.Lock({ acc_num: row.recUpd.acc_num }).SingleOrDefault();
 		let b = db.balance.Lock({ acc_num: row.recUpd.acc_num }).SingleOrDefault();
 		Log(["locked", th]);
@@ -38,11 +42,11 @@
 		if (th != null && b != null) {
 			let accOld = th;
 			let accUpd = {
-				acc_num: row.recUpd.acc_num, holder_name: row.recUpd.holder_name, balance: row.recUpd.balance,
+				acc_num: row.recUpd.acc_num, holder_name: row.recUpd.holder_name,
 				valid: row.recUpd.valid, ver: accOld.ver + 1
 			};
 
-			if (accOld.holder_name != accUpd.holder_name || b.balance != accUpd.balance
+			if (accOld.holder_name != accUpd.holder_name || b.balance != row.recUpd.balance
 				|| accOld.valid != accUpd.valid) {
 				if (row.rec.ver != accOld.ver) {
 					// prevent overwrite
@@ -50,6 +54,7 @@
 				} else {
 					Log(["update: ", accUpd.acc_num, " to ", accUpd]);
 					db.account.Update({ acc_num: accUpd.acc_num }, accUpd);
+					db.balance.Update({ acc_num: accUpd.acc_num }, {balance: row.recUpd.balance, ver: b.ver + 1});
 				}
 			}
 		}
